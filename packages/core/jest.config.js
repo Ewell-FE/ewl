a3f4c116052fa709fd2b0d3ababcfd6395d4e94a
lileilei <lileilei@ewell.cc>
2020-06-03 20:28:09 +0800

    tsconfig

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,4 +1,4 @@
-const config = require('ew-material-configs/jest.config')
+const config = require('@ewl/ts-configs/jest.config')
 module.exports = Object.assign({}, config, {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
