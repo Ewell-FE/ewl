d6850ee10582a9c6288faa062c8b89254c29e5af
lileilei <lileilei@ewell.cc>
2020-11-04 13:45:37 +0800

    readme

--- README.md
+++ README.md
@@ -2,4 +2,13 @@ Ewl Material
 =========================
  站在巨人的肩膀上!
  基于material-ui 打造一套 企业级的 高效易用组件
+ 
+ 
+ Module Federation
+ =========================
+ 模块联邦暂时对服务端渲染支持有限，所以加载外部模块都采用如下引入方式
+ ```
+ const RemoteTitle = dynamic(import('next1/exposedTitle'), {ssr: false});
+ ```
+Next官方并未升级到webpack5且暂时与react-redux 等库存在兼容性问题
 
--- packages/examples/client/pages/other.tsx
+++ packages/examples/client/pages/other.tsx
@@ -5,7 +5,7 @@ const RemoteTitle = dynamic(import('next1/exposedTitle'), {ssr: false});
 const OtherPage = () => (
     <div>
         <h1>Other Page</h1>
-        <p>this is module federation , the module come from nextjs project of module-federation-examples </p>
+        <p>this is module federation , the module comes from nextjs project of module-federation-examples </p>
         <RemoteTitle/>
         <Link href="/">
             <a>Get back to "/"</a>
